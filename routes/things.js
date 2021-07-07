404005021184de28b0b1054adad0b7f82c34e447
Samah <samahalimuhammad@gmail.com>
2021-07-07 07:00:36 +0200

    Create endpoint with query

--- routes/things.js
+++ routes/things.js
@@ -7,11 +7,19 @@ const thingsRef = db.collection('things');
 
 // read all docs inside things collection
 
-router.get('/', async (req,res)=>{
-    const snapshot =  await thingsRef.get();
-    snapshot.forEach(doc => {
-  console.log(doc.id, '=>', doc.data());
+router.get('/', async (req, res) => {
+  let snapshot = await thingsRef.get();
+  snapshot.forEach(doc => {
+    console.log(doc.id, '=>', doc.data());
+  });
+  snapshot = await thingsRef.where("name", "==", "apple")
+    .where("number", ">", 3)
+    .orderBy("number").get();
+  snapshot.forEach(doc => {
+    let data = doc.data();
+    console.log(data.name ,doc.get('number'));
+  });
+  return res.json({});
+
 });
-    
-})
 module.exports = router;
